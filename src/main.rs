04e88af28e8fe26081ceb45f5c296e7ec82e8be4
Axolotl <rishavghosh2007@gmail.com>
2024-07-27 17:41:44 +0530

    Trivial change to error handling

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,21 @@ use std::process;
 
 fn main() {
     let config = QueryType::new().unwrap_or_else(|error| {
-        eprintln!("{}", error);
+        eprintln!("Error occured: {}", error);
         process::exit(1);
     });
     
 match config{
         QueryType::Dictionary(word) => {
             let word_info = get_json(&word).unwrap_or_else(|error| {
-                eprintln!("{}", error);
+                eprintln!("Error occured{}", error);
                 process::exit(1);
             });
             word_info.dictionary();
         },
         QueryType::Thesaurus(word) => {
             let word_info = get_json(&word).unwrap_or_else(|error| {
-                eprintln!("{}", error);
+                eprintln!(" Error ocurred: {}", error);
                 process::exit(1);
             });
             word_info.thesaurus();
